c7525be8b0a4805a539ced136c78e5b5d0068b79
tjphillips08 <tjphillips08@icloud.com>
2025-03-23 13:11:52 -0500

    Initializing springboot and java application

---
+++ src/main/java/com/example/photography_backend/PhotographyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.photography_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotographyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photography_backend/PhotographyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photography_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
